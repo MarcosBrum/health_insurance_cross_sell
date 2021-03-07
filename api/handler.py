34469e127e614ebd3d5794a2536fb045d30e6fdd
MarcosBrum <marcos.brum@gmail.com>
2021-03-07 12:47:25 -0300

    API prepared

---
+++ api/handler.py
@@ -0,0 +1,58 @@
+# %% markdown
+# Import Modules
+
+# %% codecell
+import pandas as pd
+from flask import Flask, request, Response
+from cross_sell.cross_sell import CrossSell
+# o pacote é a pasta. Deste, quer-se o Arquivo, e importa-se a Classe.
+import pickle
+
+
+# %% markdown
+# Load model
+
+# %% codecell
+model = pickle.load(open('/home/marcos/Documentos/comunidade_DS/pa004_health_insurance_cross_sell/ml_models_comp/xgb_validated_new.pkl', 'rb'))
+
+
+# %% markdown
+# API
+
+# %% codecell
+app = Flask(__name__)
+
+
+@app.route('/cross_sell/predict', methods=['POST'])
+def rossmann_predict():
+    test_json = request.get_json()
+
+    if test_json:
+        if isinstance(test_json, dict):  # unique example
+            test_raw = pd.DataFrame(test_json, index=[0])
+        else:  # multiple examples
+            test_raw = pd.DataFrame(test_json, columns=test_json[0].keys())
+
+        # instantiate Rossmann class
+        pipeline = CrossSell()
+
+        # data cleaning
+        df1 = pipeline.data_cleaning(test_raw)
+
+        # feature engineering
+        df2 = pipeline.feature_engineering(df1)
+
+        # data preparation
+        df3 = pipeline.data_preparation(df2)
+
+        # prediction
+        df_response = pipeline.get_prediction(model, test_raw, df3)
+
+        return df_response
+
+    else:
+        return Response('{}', status=200, mimetype='application/json')
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0')
---
+++ webapp/cross_sell/cross_sell.py
@@ -0,0 +1,96 @@
+# %% markdown
+# Import Modules
+
+# %% codecell
+import pandas as pd
+import pickle
+
+
+# %% markdown
+# Class
+
+# %% codecell
+class CrossSell:
+    def __init__(self):
+        self.rs_age = pickle.load(open("encoders/age_new_scaler.pkl", "rb"))
+        self.region_freq = pickle.load(open("encoders/region_freq_new_scaler.pkl", "rb"))
+        self.channel_freq = pickle.load(open("encoders/channel_freq_new_scaler.pkl", "rb"))
+        self.rs_premium = pickle.load(open("encoders/premium_new_scaler.pkl", "rb"))
+        self.mms_vintage = pickle.load(open("encoders/vintage_new_scaler.pkl", "rb"))
+        self.age_encoder = pickle.load(open("encoders/v_age_new_encoder.pkl", "rb"))
+        self.damage_encoder = pickle.load(open("encoders/v_dam_new_encoder.pkl", "rb"))
+
+    def data_cleaning(self, df1):
+        # lowercase columns
+        cols_list = df1.columns.tolist()
+        cols_lower = [x.lower() for x in cols_list]
+        df1.columns = cols_lower
+
+        # data types
+        df1['region_code'] = df1['region_code'].astype(int)
+        df1['policy_sales_channel'] = df1['policy_sales_channel'].astype(int)
+        return df1
+
+    def feature_engineering(self, df2):
+        def damage_map(damage):
+            if damage == 'Yes':
+                return 1
+            else:
+                return 0
+
+        vehicle_hist = df2['vehicle_damage'].map(damage_map) + 1 - df2['previously_insured']
+
+        df2.insert(loc=len(df2.columns)-1, column='vehicle_hist', value=vehicle_hist)
+        return df2
+
+    def data_preparation(self, df9):
+        df9 = df9.copy()
+
+        # apply encoders and scalers
+        #gender: "One-hot" encoder
+        df9 = pd.get_dummies(df9, prefix='gender', columns=['gender'])
+
+        # age - RobustScaler
+        df9['age'] = self.rs_age.transform(df9[['age']].values)
+
+        # region_code - frequency encoding
+        df9['region_code'] = df9['region_code'].map(self.region_freq)
+
+        # policy_sales_channel - frequency encoding
+        df9['policy_sales_channel'] = df9['policy_sales_channel'].map(self.channel_freq)
+
+        # annual_premium - RobustScaler
+        df9['annual_premium'] = self.rs_premium.transform(df9[['annual_premium']].values)
+
+        # vintage - MinMaxScaler
+        df9['vintage'] = self.mms_vintage.transform(df9[['vintage']].values)
+
+        # vehicle_age - LabelEncoder
+        df9['vehicle_age'] = self.age_encoder.transform(df9['vehicle_age'])
+
+        # vehicle_damage - LabelEncoder
+        df9['vehicle_damage'] = self.damage_encoder.transform(df9['vehicle_damage'])
+
+        # select columns
+        cols_selected_full = ['id', 'age', 'policy_sales_channel', 'previously_insured', 'annual_premium', 'vintage',
+                              'vehicle_hist', 'gender_Female', 'gender_Male']
+        df9 = df9[cols_selected_full].copy()
+        return df9
+
+    def get_prediction(trained_model, data_test):
+        # drop id
+        data_testing = data_test.drop(['id'], axis=1).copy()
+        # predict_proba:
+        yhat_proba = trained_model.predict_proba(data_testing)
+
+        # transform yhat_proba to 1D-array
+        yhat_proba_1d = yhat_proba[:, 1].tolist()
+
+        # include in dataframe
+        testing_data = data_test.copy()
+        testing_data['score'] = yhat_proba_1d
+        # sort
+        testing_data = testing_data.sort_values('score', ascending=False)
+        # reset index
+        testing_data.reset_index(drop=True, inplace=True)
+        return testing_data
